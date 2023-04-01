49f13a7ebf73e2a3c24794d160f8c71498e0e910
Kenref <107287396+Kenref@users.noreply.github.com>
2023-04-01 14:58:15 +1100

    Implemented the get getNumber function for button clicks

--- script.js
+++ script.js
@@ -16,74 +16,42 @@ function operate(a, operator, b) {
 let outputBox = document.getElementById("outputBox");
 const number = document.getElementsByClassName("number");
 const operator = document.getElementsByClassName("operator");
+const equalsButton = document.getElementById("equals")
 
-let a = 0, middle = "", b = 0, runningTotal = 0
+
+let a = { value: "" }, middle = { value: ""}, b = { value: ""}, runningTotal = 0
 
 function getNumber(num) {
     for (let i = 0; i < number.length; i++) {
         number[i].addEventListener("click", (e) => {
-            if (outputBox.textContent == 0) {
-                outputBox.textContent = e.srcElement.innerText
-                numbers = e.srcElement.innerText
-            }else {
-                outputBox.textContent += e.srcElement.innerText
-                numbers += e.srcElement.innerText
-            }
-            console.log(numbers)
-            return num
+            outputBox.textContent += e.target.classList[1]
+            num.value += e.target.classList[1]
+            // console.log(num.value)
         })
     }
+    return num
 }
 
-function getOperator() {
-    for (let i = 0; i < operator.length; i++) {
-        operator[i].addEventListener("click", (e) => {
-            if (outputBox.textContent == 0) {
-                outputBox.textContent = e.srcElement.innerText
-            }else {
-                outputBox.textContent += e.srcElement.innerText
-            }
 
-        })
-    }
+
+
+a = getNumber(a)
+
+function equals(abc) {
+    equalsButton.addEventListener("click", () => {
+        console.log(abc.value)
+    })
 }
+equals(a)
+
+
 
 // find a way to append first into the runningTotal when middle is activated
 // need to add another event listenrs
 // need to bake conditionals into getnumber
 
-if (a == 0 && middle == "") {
-    a = getNumber(a)
-    console.log("hi")
-}
-if (middle == "") {
-    middle = getOperator()
-    console.log("yes")
-}
-if (middle != "" || a != 0) {
-    b = getNumber(b)
-}
 
-
-// let first = getNumber(a)
-// console.log(first)
-
-// let middle = getOperator()
-
-
-// let second = getNumber(b)
-
-
-// console.log(first, second)
 
 
 
 // add functionality to the "other" class buttons
-
-
-// number.addEventListener("click", (e) => {
-//     outputBox.textContent += e.explicitOriginalTarget.innerText
-//     console.log(e)
-//     console.log(e.explicitOriginalTarget.innerText)
-
-// })
