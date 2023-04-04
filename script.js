aa77e6ef3a07da500445ec536e49ea8a3e471174
Kenref <107287396+Kenref@users.noreply.github.com>
2023-04-04 17:00:23 +1000

    Refactored functions by moving event listeners outside of all functions
    Rewrote getNumber functions
    added clear functionality
    added equals functionality
    fixed up operator buttons to allow for consecutive operators

--- script.js
+++ script.js
@@ -14,40 +14,67 @@ function operate(a, operator, b) {
 }
 
 let outputBox = document.getElementById("outputBox");
-const number = document.getElementsByClassName("number");
-const operator = document.getElementsByClassName("operator");
+const numberButton = document.querySelectorAll(".number");
+const operatorButton = document.querySelectorAll(".operator");
 const equalsButton = document.getElementById("equals")
+const clearButton = document.getElementById("clear")
+
+let a = "", middle = "", b = "", runningTotal = 0
+let operatorClicked = false
+
+numberButton.forEach(button => {
+    button.addEventListener("click", e => {
+        if (operatorClicked) {
+          b += e.target.classList[1];
+          outputBox.textContent += e.target.classList[1];
+        } else {
+            a += e.target.classList[1]
+            outputBox.textContent += e.target.classList[1];
+        }
+    });
+});
+
+operatorButton.forEach(button => {
+    button.addEventListener("click", e => {
+        if (b === "") {
+            middle = e.target.classList[1];
+            outputBox.textContent += middle;
+        } else {
+            runningTotal = operate(Number(a), middle, Number(b));
+            outputBox.textContent = runningTotal;
+            a = runningTotal;
+            b = "";
+            middle = e.target.classList[1];
+            outputBox.textContent += middle;
+        }
+        operatorClicked = true;
+    });
+});
+  
 
 
-let a = { value: "" }, middle = { value: ""}, b = { value: ""}, runningTotal = 0
+clearButton.addEventListener("click", e => {
+    a = ""
+    middle = ""
+    b = ""
+    operatorClicked = false
+    outputBox.textContent = ""
+})
 
-function getNumber(num) {
-    for (let i = 0; i < number.length; i++) {
-        number[i].addEventListener("click", (e) => {
-            outputBox.textContent += e.target.classList[1]
-            num.value += e.target.classList[1]
-            // console.log(num.value)
-        })
-    }
-    return num
+
+function equals() {
+    runningTotal =  operate(Number(a), middle, Number(b))
+    outputBox.textContent = runningTotal
+    a = runningTotal
+    b = ""
 }
 
-
-
-
-a = getNumber(a)
-
-function equals(abc) {
-    equalsButton.addEventListener("click", () => {
-        console.log(abc.value)
-    })
-}
-equals(a)
-
-
+equalsButton.addEventListener("click", () => {
+    equals()
+  });
 
 // find a way to append first into the runningTotal when middle is activated
-// need to add another event listenrs
+// need to add another e listenrs
 // need to bake conditionals into getnumber
 
 
